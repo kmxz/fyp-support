7e706e97e191e39d494e5f647523eb33fea4bb95
kmxz <kxzkxz7139@gmail.com>
2015-03-22 15:26:56 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+var nodeStatic = require('node-static');
+var http = require('http');
+var ws = require('ws');
+
+// port 10000: static file - serving static files in this directory
+
+var fileServer = new nodeStatic.Server(__dirname);
+
+http.createServer(function (request, response) {
+  request.addListener('end', function () {
+    fileServer.serve(request, response);
+  }).resume();
+}).listen(10000); 
+
+// port 10010: websockets - allow users to subscribe
+
+var wss = new ws.Server({ port: 10010 });
+
+// port 10020: receives python uploads, and directly forward to websockets clients
+
+http.createServer(function(request, response) {
+  var str = '';
+  request.on('data', function (data) {
+    str += data;
+  });
+  request.on('end', function () {
+    wss.clients.forEach(function (client) {
+      client.send(str);
+    });
+    console.log('A message forward to ' + wss.clients.length + ' clients!');
+    response.end();
+  });
+}).listen(10020);
+
+// that's all!
+
+console.log('All three servers started!');
