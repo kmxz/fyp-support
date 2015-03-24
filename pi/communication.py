343cb7f484646546fd8a1c4ee7fb6aee2cc28faa
kmxz <kxzkxz7139@gmail.com>
2015-03-24 13:36:28 +0800

    写不下去了

--- pi/communication.py
+++ pi/communication.py
@@ -5,6 +5,13 @@ import threading
 
 REMOTE_HOST = 'dmc632.resnet.ust.hk:10020'
 
+def buffered_to_data(thread):
+    thread.lock.acquire()
+    out = thread.buf
+    thread.buf = []
+    thread.lock.release()
+    return out
+
 class Communication(threading.Thread):
 
     def __init__(self, usss, qr):
@@ -12,14 +19,7 @@ class Communication(threading.Thread):
         self.usss = usss
         self.qr = qr
 
-    def buffed_to_data(thread):
-        thread.lock.acquire()
-        out = thread.buf
-        thread.buf = []
-        thread.lock.release()
-        return out
-
     def run(self):
         while True:
-            post_data = {'time': time.time(), 'ultrasonic': map(self.buffed_to_data, self.usss), 'qr': self.buffed_to_data(self.qr)}
+            post_data = {'time': time.time(), 'ultrasonic': map(buffered_to_data, self.usss), 'qr': self.buffed_to_data(self.qr)}
             r = requests.post('http://' + REMOTE_HOST + '/', data=json.dumps(post_data))
